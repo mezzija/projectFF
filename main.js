c6f65c4676b70ae5dde05f8857ceebdce9530fca
mezzija <mezzija@mail.ru>
2020-06-19 16:19:50 +0300

    result main page

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const nav=document.getElementById('nav');
 const menu=document.getElementById('nav_menu');
-console.log(nav);
+
+const logoMenu = document.querySelectorAll('.header_icon');
+
+
 menu.addEventListener('click',(event)=>{
     event.preventDefault();
     nav.classList.toggle('nav_active');
+    logoMenu.forEach(item=>item.classList.remove('header_active'))
+    if(nav.classList.contains('nav_active')){
+        logoMenu[1].classList.add('header_active')
+    }else{
+        logoMenu[0].classList.add('header_active')
+    }
 })
